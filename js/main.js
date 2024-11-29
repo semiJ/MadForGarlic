70fb314b9ff3b9adba268314dc926a9e8d1f1f3b
semeJ <04tpal04@gmail.com>
2024-11-29 16:39:49 +0900

    매드포갈릭

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$(function(){
-    let baseline = -600;
+// $(function(){
+//     let baseline = -600;
 
-    let banner = $("#banner").offset().top + baseline;
-    let mainMenu = $("#mainMenu").offset().top + baseline;
-    let mainBrand = $("#mainBrand").offset().top + baseline;
-    let mainMembership = $("#mainMembership").offset().top + baseline;
-    let mainEvent = $("#mainEvent").offset().top + baseline;
-    let mainCustomer = $("#mainCustomer").offset().top + baseline;
-    let ft = $("footer").offset().top + baseline;
-})
+//     let banner = $("#banner").offset().top + baseline;
+//     let mainMenu = $("#mainMenu").offset().top + baseline;
+//     let mainBrand = $("#mainBrand").offset().top + baseline;
+//     let mainMembership = $("#mainMembership").offset().top + baseline;
+//     let mainEvent = $("#mainEvent").offset().top + baseline;
+//     let mainCustomer = $("#mainCustomer").offset().top + baseline;
+//     let ft = $("footer").offset().top + baseline;
+// })
