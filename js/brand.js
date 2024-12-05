51c1fece5b9f134770d77dc74538ad61f0bd7dd1
semeJ <04tpal04@gmail.com>
2024-12-05 17:55:29 +0900

    매드포갈릭

--- js/brand.js
+++ js/brand.js
@@ -1,9 +1,15 @@
 $(function() {
 
+    // brandnews 자동 슬라이드
     let timer = setInterval(function() {
         $(".brandNewsDetail ul").stop().animate({"margin-top": "-185px"}, 1000, function() {
             $(".brandNewsDetail li").first().appendTo(".brandNewsDetail ul");
             $(".brandNewsDetail ul").css({"margin-top": "0px"});
         });
     }, 3000);
+
+
+    // brandconcept 움직임
+    
+
 });
