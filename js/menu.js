7688a60c651ec4653f593cd9963b1f473712c378
semeJ <04tpal04@gmail.com>
2024-12-06 14:42:13 +0900

    매드포갈릭

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,11 @@ $(function(){
     $(".menuBtnDetail li").on("click", function(){
         let i = $(this).index();
 
-        $(".mainMenuImg li").removeClass("m_on");
-        $(".mainMenuImg li").eq(i).addClass("m_on");
-
         $(".mainMenuImg li h3").removeClass("menuTitle");
         $(".mainMenuImg li h3").eq(i).addClass("menuTitle");
+
+        $(".mainMenuImg li").removeClass("m_on");
+        $(".mainMenuImg li").eq(i).addClass("m_on");
 
         $(".menuBtnDetail li").removeClass("m_btn");
         $(".menuBtnDetail li").eq(i).addClass("m_btn");
