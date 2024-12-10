e9cb412ca43c0d9ece687fafd7be7f8d5799b8ec
semeJ <04tpal04@gmail.com>
2024-12-10 14:14:43 +0900

    포트폴리오

--- js/membership.js
+++ js/membership.js
@@ -1,5 +1,25 @@
-$(function(){
+$(function () {
+    let viewHeight = document.querySelector('html').clientHeight;
+        const content = document.querySelectorAll('.content');
+        const img = document.querySelectorAll('.img');
 
-    
+        window.addEventListener('scroll',function(){
+        if(content[0].getBoundingClientRect().top < viewHeight/2){
+        img[0].classList.add('opacity1');
+        }
 
-})
+        for(i = 1; i < content.length - 1; i++){
+        if(content[i].getBoundingClientRect().top < viewHeight/2 && content[i].getBoundingClientRect().top > -viewHeight/2){
+            img[i].classList.add('opacity1');
+        } else{
+            img[i].classList.remove('opacity1');
+        }
+        }
+
+        if(content[content.length - 1].getBoundingClientRect().top < viewHeight/2){
+        img[content.length - 1].classList.add('opacity1');
+        } else{
+        img[content.length - 1].classList.remove('opacity1');
+        }
+        });
+});
