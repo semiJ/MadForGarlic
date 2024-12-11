60c511caa38a2398b5c8f39412005e77bdee1188
semeJ <04tpal04@gmail.com>
2024-12-11 17:48:03 +0900

    매드포갈릭

--- js/event.js
+++ js/event.js
@@ -1,3 +1,22 @@
-$(function(){
-   
-})
+$(function() {
+    let imageWidth = $(".eveTXT li img").outerWidth();
+    let gap = 40;
+
+    // 애니메이션 시작
+    setInterval(function() {
+        $(".eveTXT").css("transition", "transform 2s ease-in-out");
+        $(".eveTXT").css("transform", "translateX(-" + (imageWidth + gap) + "px)");
+
+        setTimeout(function() {
+            $(".eveTXT li:first-child").appendTo(".eveTXT");  
+            $(".eveTXT").css("transition", "none");
+            $(".eveTXT").css("transform", "translateX(0)"); 
+
+            setTimeout(function() {
+                $(".eveTXT").css("transition", "transform 2s ease-in-out");
+            }, 50); 
+        }, 3000); 
+    }, 4000);
+
+    
+});
